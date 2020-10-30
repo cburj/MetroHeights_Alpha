486e0bb66d83813a3ecb46be7b6a2dc1aee51190
cburj <charlieburgess1998@gmail.com>
2020-10-30 23:12:28 +0000

    Upaded some skybox stuff and re-organised the file structure

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/EnemyStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* SOURCES: */
+/* http://answers.unity.com/answers/260874/view.html*/
+/* https://docs.unity3d.com/ScriptReference/ParticleSystem.MainModule-loop.html*/
+
+public class EnemyStats : MonoBehaviour
+{
+    public float hp = 100.0f;
+    public GameObject explosion;
+
+    public void Kill()
+    {
+        //Pass in the particle system prefab as a game object.
+        GameObject explosionEffect = Instantiate(explosion)
+                                         as GameObject;
+
+        //Set its position to the position of the enemy.
+        explosionEffect.transform.position = transform.position;
+
+        //Create a new reference to the particle system on the game object.
+        ParticleSystem expParticles = explosionEffect.GetComponent<ParticleSystem>();
+
+        //Make a new reference/variable that points to the "main" variable of the PS.
+        var main = expParticles.main;
+
+
+        //Prevent looping
+        main.loop = false;
+        //Play the PS
+        expParticles.Play();
+
+        //Destroy the PS once its finished
+        Destroy(explosionEffect.gameObject, main.duration);
+
+        //Destroy the enemy
+        Destroy(gameObject);
+
+    }
+}
