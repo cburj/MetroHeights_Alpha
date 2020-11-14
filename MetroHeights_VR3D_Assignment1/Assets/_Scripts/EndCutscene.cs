85bfbebdce0bc53c58c7f0f7a40c37f6a00d02ed
cburj <charlieburgess1998@gmail.com>
2020-11-14 16:02:08 +0000

    Adding foundation for cutscenes. Need to compress intro to fit in repo :smile:

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/EndCutscene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class EndCutscene : MonoBehaviour
+{
+    public VideoPlayer video;
+
+    void Start()
+    {
+        StartCoroutine("checkCutscene");
+    }
+
+    IEnumerator checkCutscene()
+    {
+        while(( video.frame) <= 0 || (video.isPlaying == true))
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Level01");
+    }
+}
