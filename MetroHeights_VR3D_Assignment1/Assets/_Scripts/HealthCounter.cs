531d392aa8a1737f2a3f453d899d44edad2b6731
cburj <charlieburgess1998@gmail.com>
2020-12-09 16:46:08 +0000

    Player Health UI Element

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/HealthCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private float healthSize;
+    private float health;
+
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = transform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSize = player.transform.GetComponent<PlayerStats>().maxHealth;
+        health = player.transform.GetComponent<PlayerStats>().currentHealth;
+
+        text.text = health + "/" + healthSize +"HP";
+    }
+}
