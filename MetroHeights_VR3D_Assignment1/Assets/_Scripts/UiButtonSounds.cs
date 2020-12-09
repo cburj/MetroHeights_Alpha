ecf10db5f037b5fba78f0cf4b46c15f719ea4bb5
cburj <charlieburgess1998@gmail.com>
2020-12-09 16:31:20 +0000

    UI Hover Sounds!

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/UiButtonSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/* https://gamedev.stackexchange.com/questions/116801/how-can-i-detect-that-the-mouse-is-over-a-button-so-that-i-can-display-some-ui-t*/
+
+public class UiButtonSounds : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public AudioClip hoverSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData pointer)
+    {
+        audioSource.clip = hoverSound;
+        audioSource.Play();
+    }
+
+    public void OnPointerExit(PointerEventData pointer)
+    {
+    }
+}
