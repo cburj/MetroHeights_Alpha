4bd63e61967a57605ca4dca5551be5a304c34b33
cburj <charlieburgess1998@gmail.com>
2021-01-04 12:01:31 +0000

    Global Volume Slider w/PlayerPref Defaults

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/MasterVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterVolume : MonoBehaviour
+{
+    void Update()
+    {
+        if(!PlayerPrefs.HasKey("PREF_MasterVolume"))
+        {
+            AudioListener.volume = 1.0f;
+            PlayerPrefs.SetFloat("PREF_MasterVolume", 1.0f);
+        }
+        else
+            AudioListener.volume = PlayerPrefs.GetFloat("PREF_MasterVolume");
+    }
+}
--- MetroHeights_VR3D_Assignment1/Assets/_Scripts/SettingsValues.cs
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/SettingsValues.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class SettingsValues : MonoBehaviour
 {
     Toggle toggle;
-    
+
     private void Start()
     {
         toggle = gameObject.GetComponent<Toggle>();
 
+        /* DEFAULT VALUES */
+        PlayerPrefs.SetFloat("PREF_MasterVolume", 1.0f);
+        PlayerPrefs.SetInt("PREF_RainFX", 1);
+        PlayerPrefs.SetInt("PREF_PostFX", 1);
     }
+
     public void RainFX()
     {
-        if(toggle.isOn)
+        if (toggle.isOn)
             PlayerPrefs.SetInt("PREF_RainFX", 1);
         else
             PlayerPrefs.SetInt("PREF_RainFX", 0);
@@ -23,10 +28,15 @@ public class SettingsValues : MonoBehaviour
 
     public void PostFX()
     {
-        if(toggle.isOn)
+        if (toggle.isOn)
             PlayerPrefs.SetInt("PREF_PostFX", 1);
         else
             PlayerPrefs.SetInt("PREF_PostFX", 0);
         PlayerPrefs.Save();
+    }
+
+    public void VolSet(Slider volSlider)
+    {
+        PlayerPrefs.SetFloat("PREF_MasterVolume", volSlider.value);
     }
 }
