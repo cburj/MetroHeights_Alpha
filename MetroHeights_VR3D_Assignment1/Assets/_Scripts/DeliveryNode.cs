efd1063e7863a4b3a1673fb1330a04920fd5c425
cburj <charlieburgess1998@gmail.com>
2021-01-07 14:32:30 +0000

    Delivery Node setup

---
+++ MetroHeights_VR3D_Assignment1/Assets/_Scripts/DeliveryNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryNode : MonoBehaviour
+{
+    public GameObject prompt;
+    public string DeliveryNodeTag;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if ((col.gameObject.tag == DeliveryNodeTag) )
+        {
+            prompt.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if ((col.gameObject.tag == DeliveryNodeTag))
+        {
+            prompt.SetActive(false);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && prompt.activeSelf)
+        {
+            Debug.Log("YOU HAVE DELIVERED AN ITEM");
+        }
+    }
+}
